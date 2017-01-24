0e7a49c1f891b4fd2ef234da3780794e937814bf
driera_wam <driera@wearemarketing.com>
2017-01-24 17:58:17 +0100

    updates postcss packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,20 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var vars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var pxtorem = require('postcss-pxtorem');
 var sourcemaps = require('gulp-sourcemaps');
-var livereload = require('gulp-livereload');
+var autoprefixer = require('autoprefixer');
+var cssnext = require('cssnext');
+var precss = require('precss');
 
 var frontend = './app/';
 var dest = './web/';
 
 gulp.task('css', function() {
     var processors = [
-        vars,
-        nested,
-        pxtorem({
-            root_value: 16,
-            unit_precision: 2,
-            prop_white_list: ['font-size', 'line-height', 'padding'],
-            replace: true,
-            media_query: false
-        }),
         autoprefixer({
             browsers: ['last 2 version']
-        })
+        }),
+        cssnext,
+        precss
     ];
 
     return gulp.src(frontend + 'styles/*.css')
@@ -31,13 +22,9 @@ gulp.task('css', function() {
         .pipe(postcss(processors))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(dest + 'css/'))
-        .pipe(livereload());
 })
 
 gulp.task('watch', function() {
-    livereload.listen({
-        start: true
-    });
     gulp.watch(frontend + '**/*.css', ['css']);
 });
 
