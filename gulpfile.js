62086beb72ec7132618516aa666a4089119000ec
driera_wam <driera@wearemarketing.com>
2017-01-25 15:17:03 +0100

    adds browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,71 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('autoprefixer');
 var cssnext = require('cssnext');
 var precss = require('precss');
+var px2Rem = require('postcss-pxtorem');
+var nano = require('cssnano');
+pjson = require('./package.json');
 
-var frontend = './app/';
-var dest = './web/';
+var defaultNotification = function(err) {
+    return {
+        subtitle: err.plugin,
+        message: err.message,
+        sound: 'Funk',
+        onLast: true,
+    };
+};
+
+var config = Object.assign({}, pjson.config, defaultNotification);
+
+gulp.task('default', ['css', 'server', 'watch']);
+
+// SERVER
+
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: config.destUrl
+        },
+        open: false,
+        port: config.port || 3000
+    });
+});
+
+// WATCH
+
+gulp.task('watch', function() {
+    gulp.watch(config.origUrl + '/**/*.css', ['css']);
+    gulp.watch(config.destUrl + '/**/*.html', ['html']);
+});
+
+// HTML
+
+gulp.task('html', function() {
+  gulp.src(config.destUrl + '/*.html')
+    .pipe(gulp.dest(config.destUrl))
+    .pipe(browserSync.stream());
+});
+
+// CSS
 
 gulp.task('css', function() {
     var processors = [
+        cssnext,
+        precss,
+        px2Rem,
         autoprefixer({
             browsers: ['last 2 version']
         }),
-        cssnext,
-        precss
+        nano
     ];
 
-    return gulp.src(frontend + 'styles/*.css')
+    return gulp.src(config.origUrl + '/styles/*.css')
         .pipe(sourcemaps.init())
         .pipe(postcss(processors))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(dest + 'css/'))
+        .pipe(gulp.dest(config.destUrl + '/css/'))
+        .pipe(browserSync.stream());
 })
-
-gulp.task('watch', function() {
-    gulp.watch(frontend + '**/*.css', ['css']);
-});
-
-gulp.task('default', ['css', 'watch']);
