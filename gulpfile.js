d813d6d9b414376eb0ec98e1354bff08278351b1
Dani Riera <info@driera.com>
2017-02-05 10:33:38 +0100

    changes gulpfile packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,47 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var vars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var pxtorem = require('postcss-pxtorem');
-var sourcemaps = require('gulp-sourcemaps');
-var livereload = require('gulp-livereload');
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssimport = require('postcss-import'),
+    customproperties = require('postcss-custom-properties'),
+    apply = require('postcss-apply'),
+    mixins = require('postcss-mixins'),
+    nested = require('postcss-nested'),
+    customMedia = require('postcss-custom-media'),
+    nano = require('gulp-cssnano'),
+    notify = require('gulp-notify'),
+    // vars = require('postcss-simple-vars'),
+    // pxtorem = require('postcss-pxtorem'),
+    sourcemaps = require('gulp-sourcemaps'),
+    livereload = require('gulp-livereload');
 
 var frontend = './app/';
 var dest = './web/';
 
 gulp.task('css', function() {
     var processors = [
-        vars,
+        cssimport,
+        autoprefixer,
+        customproperties,
+        apply,
+        mixins,
         nested,
-        pxtorem({
-            root_value: 16,
-            unit_precision: 2,
-            prop_white_list: ['font-size', 'line-height', 'padding'],
-            replace: true,
-            media_query: false
-        }),
-        autoprefixer({
-            browsers: ['last 2 version']
-        })
+        customMedia
     ];
+
+    var configNano = {
+        autoprefixer: { browsers: ['last 2 version']},
+        discardComments: { removeAll: true },
+        safe: true
+    };
 
     return gulp.src(frontend + 'styles/*.css')
         .pipe(sourcemaps.init())
         .pipe(postcss(processors))
         .pipe(sourcemaps.write('.'))
+        .pipe(nano(configNano))
         .pipe(gulp.dest(dest + 'css/'))
-        .pipe(livereload());
+        .pipe(livereload())
+        .pipe(notify({ message: 'CSS Ready' }));
 })
 
 gulp.task('watch', function() {
