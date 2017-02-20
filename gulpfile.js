085c436abe75bd846e1ab5feeaf0f5bc65a8c534
Dani Riera <driera@wearemarketing.com>
2017-02-20 16:52:37 +0100

    adds svg tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp'),
-    postcss = require('gulp-postcss'),
+    $ = require('gulp-load-plugins')(),
+    svgo = require('gulp-svgo'),
+    svgSymbols = require('gulp-svg-symbols'),
+    nano = require('cssnano'),
+    notify = require('gulp-notify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browserSync = require('browser-sync').create(),
+    pjson = require('./package.json');
+
+var postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     cssimport = require('postcss-import'),
     customproperties = require('postcss-custom-properties'),
@@ -7,11 +16,32 @@ var gulp = require('gulp'),
     mixins = require('postcss-mixins'),
     nested = require('postcss-nested'),
     customMedia = require('postcss-custom-media'),
-    nano = require('cssnano'),
-    notify = require('gulp-notify'),
-    sourcemaps = require('gulp-sourcemaps'),
-    browserSync = require('browser-sync').create(),
-    pjson = require('./package.json');
+    minmax = require('postcss-media-minmax'),
+    cssFor = require('postcss-for'),
+    color = require('postcss-color-function');
+
+var config = Object.assign({}, pjson.config, defaultNotification);
+
+var processors = [
+    cssimport,
+    customproperties,
+    apply,
+    mixins,
+    nested,
+    customMedia,
+    minmax,
+    cssFor,
+    color,
+    autoprefixer({
+        browsers: config.browserslist
+    }),
+    nano({
+        discardComments: {
+            removeAll: true
+        },
+        safe: true
+    })
+];
 
 var defaultNotification = function(err) {
     return {
@@ -22,9 +52,7 @@ var defaultNotification = function(err) {
     };
 };
 
-var config = Object.assign({}, pjson.config, defaultNotification);
-
-gulp.task('default', ['css', 'server', 'watch']);
+gulp.task('default', ['css', 'svg', 'server', 'watch']);
 
 // SERVER
 
@@ -48,7 +76,7 @@ gulp.task('watch', function() {
 // HTML
 
 gulp.task('html', function() {
-  gulp.src(config.destUrl + '/*.html')
+    gulp.src(config.destUrl + '/*.html')
     .pipe(gulp.dest(config.destUrl))
     .pipe(browserSync.stream());
 });
@@ -56,28 +84,54 @@ gulp.task('html', function() {
 // CSS
 
 gulp.task('css', function() {
-    var processors = [
-        cssimport,
-        customproperties,
-        apply,
-        mixins,
-        nested,
-        customMedia,
-        autoprefixer({
-            browsers: ['last 2 version']
-        }),
-        nano({
-            discardComments: { removeAll: true },
-            safe: true
-        })
-    ];
-
     return gulp.src(config.origUrl + '/styles/*.css')
         .pipe(sourcemaps.init())
+        .pipe($.plumber({
+            errorHandler: defaultNotification
+        }))
         .pipe(postcss(processors))
-        .pipe(sourcemaps.write('.'))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(config.destUrl + '/css/'))
         .pipe(browserSync.stream())
         .pipe(notify({ message: 'CSS Ready' }));
 
 })
+
+// SVG
+
+gulp.task('svgo', function() {
+    return gulp.src(config.origUrl + '/svg/initial/*.svg')
+    .pipe(svgo())
+    .pipe(gulp.dest(config.origUrl + '/svg/optimized'));
+});
+
+gulp.task('svgSprite', ['svgo'], function () {
+    return gulp.src(config.origUrl + '/svg/optimized/*.svg')
+    .pipe(svgSymbols({
+        fontSize: 10,
+        title: '%f icon',
+        className: '.icon-%f',
+        id: 'icon-%f',
+        templates: ['default-svg', 'default-css', 'default-demo']
+    }))
+    .pipe($.cheerio({
+        run: function($) {
+            $('[fill]').removeAttr('fill');
+        },
+        parserOptions: { xmlMode: true }
+    }))
+    .pipe(gulp.dest(config.destUrl + '/svg/'));
+});
+
+gulp.task('svg', ['svgSprite'], function () {
+    return gulp.src(config.destUrl + '/svg/*.svg')
+    .pipe($.cheerio({
+        run: function(){
+        },
+        parserOptions: {
+            normalizeWhitespace: true
+        }
+    }))
+    .pipe(gulp.dest(config.destUrl + '/svg/'))
+    .pipe(notify({ message: "♦ SVG Ready ♦" }));
+});
